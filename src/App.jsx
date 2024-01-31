7406a3e084e515207cb1a7112464b9b3ef19f713
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-01-31 21:31:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Addtodo from './Addtodo'
+import './App.css'
+import Todos from './Todos'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Addtodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
